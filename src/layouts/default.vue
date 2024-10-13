2a370b437490b480cf64450ca44e24b505bbac3a
Dinesh11234 <88616510+Dinesh11234@users.noreply.github.com>
2024-10-13 11:40:10 +0530

    Delete src directory

--- src/layouts/default.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <NuxtPage />
-</template>
-<script setup  lang="ts">
-</script>
-<style lang="scss">
-@import "../assets/fonts/br-icons";
-</style>
--- src/middleware/auth.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    if (process.client && !localStorage.getItem('isAuthenticated')) {
-      return navigateTo('/login');
-    }
-  });
-  
